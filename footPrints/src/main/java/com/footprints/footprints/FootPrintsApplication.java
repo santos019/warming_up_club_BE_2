f718ea9bbdf9fdde2abdcea252398e97d2c65218
santos019 <santos019@naver.com>
2024-10-03 21:52:36 +0900

    chore: init

---
+++ footPrints/src/main/java/com/footprints/footprints/FootPrintsApplication.java
@@ -0,0 +1,13 @@
+package com.footprints.footprints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootPrintsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootPrintsApplication.class, args);
+    }
+
+}
---
+++ footPrints/src/test/java/com/footprints/footprints/FootPrintsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footprints.footprints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootPrintsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
